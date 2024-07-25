be508589755991226d86da5a78c4440ed6c9278a
RBuck8339 <ronanwb2015@outlook.com>
2024-07-25 00:58:06 -0400

    Added a way to remove excess anonymous players, have a slight bug where sometimes the players will be added twice. Will fix tomorrow

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -21,6 +21,8 @@ class DataPreprocesser():
         self.players = pd.DataFrame()
         self.player_stats_match = pd.DataFrame()
 
+        self.temp_df = pd.DataFrame()  # For testing
+
         self.matches_processed = list(pd.read_sql_query("SELECT match_id FROM Matches", connection).values)
 
     
@@ -604,25 +606,18 @@ class DataPreprocesser():
     
     # Clean up the dataframes before providing as input to model
     def clean(self):
-        # Since account_id may be NaN value
-        nan_rows = self.players[self.players['account_id'].isna()]
-        non_nan_rows = self.players.dropna(subset=['account_id']).drop_duplicates(subset=['account_id', 'match_id'])
-        
-        # If we have too many anonymous player rows
-        if(10 - nan_rows.shape[0] < non_nan_rows.shape[0]):
-            nan_rows = nan_rows.drop_duplicates(keep=(10-non_nan_rows.shape[0]))
-            
-        # If we don't have enough anonymous player rows
-        elif(10 - nan_rows.shape[0] > non_nan_rows.shape[0]):
-            target_num = (10 - nan_rows.shape[0] - non_nan_rows.shape[0])
-            repeats = (target_num // nan_rows.shape[0]) + 1
-            nan_rows = pd.concat([nan_rows] * repeats, ignore_index=True)
-            nan_rows = nan_rows.iloc[:target_num]
-            
-        self.players = pd.concat([non_nan_rows, nan_rows], ignore_index=True)
-        self.players.sort_values(by=['match_id'])
-        
         self.matches = self.matches.drop_duplicates(subset=['match_id'])
+        
+        for idx, row in self.matches.iterrows():
+            curr_players = self.players[self.players['match_id'] == row['match_id']]
+            curr_players = self.adjust_anon(curr_players)
+            print(f'curr_players shape is: {curr_players.shape}')
+            self.temp_df = pd.concat([self.temp_df, curr_players], ignore_index=True)
+            print(f'self.temp_df shape is: {self.temp_df.shape}')
+
+        self.temp_df.to_csv('test_temp.csv')
+
+        self.players.sort_values(by=['match_id'])
         
         nan_rows = self.player_stats_match[self.player_stats_match['account_id'].isna()]
         non_nan_rows = self.player_stats_match.dropna(subset=['account_id']).drop_duplicates(subset=['account_id', 'match_id'])
@@ -678,3 +673,24 @@ class DataPreprocesser():
         data.to_csv('test_data.csv')  # For verification; DELETE LATER
 
         return data
+    
+    
+    def adjust_anon(self, curr_players: pd.DataFrame):
+        # Since account_id may be NaN value
+        nan_rows = curr_players[curr_players['account_id'].isna()]
+        non_nan_rows = curr_players.dropna(subset=['account_id']).drop_duplicates(subset=['account_id', 'match_id'])
+        
+        # If we have too many or too few anonymous player rows
+        if(10 != nan_rows.shape[0] + non_nan_rows.shape[0]):
+            target_nan = 10 - non_nan_rows.shape[0]
+            nan_rows = nan_rows.drop_duplicates()
+            nan_rows = pd.concat([nan_rows] * target_nan, ignore_index=True)
+            nan_rows = nan_rows.iloc[:target_nan]
+            
+        curr_players = pd.concat([non_nan_rows, nan_rows], ignore_index=True)
+        
+        # Verification, likely unnecessary
+        if(curr_players.shape[0] != 10):
+            print('FOUND A CASE THAT DOES NOT WORK IN ADJUST_ANON()')
+        
+        return curr_players
