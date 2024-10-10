2464a132efddf07f8c9e6ba0cc5692e9152fcf45
RBuck8339 <ronanwb2015@outlook.com>
2024-10-10 00:02:52 -0400

    Added data formatting function for json

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -7,44 +7,50 @@ from DataPreprocessing import DataPreprocesser
 
 app = Flask(__name__)
 my_processor = DataPreprocesser()
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))  # Need to generate this
 
 
-@app.route('/', methods=["POST"])
+# Needs testing
+# Formats data for prediction based on what is in the JS table
+def format_data(data):
+    row_data = []  # List to store the row data
+
+    # Loop through each row
+    for idx, row in data.iterrows():
+        row_data.extend(row.values)
+
+    return pd.DataFrame([row_data])  # Make the data into a dataframe
+
+
+@app.route('/')
+def home():
+    return render_template('main_page.html')
+
+
+# When the button is clicked, retrieve the data from the data table and obtain a match prediction
+@app.route('/get_data', methods=["POST"])
 def runModel():
-    data = request.json  # I should be able to get the data directly from the table since it should be correct
+    data = request.json
+    print(data)
     data = pd.DataFrame(data)
-
-    # data = ...
-
-    # Add a way to get the latest testing scores from the model to show the users
-
-    y_pred = model(data)
-
-    # Return a dataframe of scores
-    tmp = {}
-    return jsonify(tmp)
+    data, res_match = format_data(data)  # Need to modify to actually format the correct data
+    res = model.predict(data)
+    results = {}
+    # Add code here to display scores, confidence, etc.
+    return jsonify(results)
 
 
 # Get random matches to populate the data table
-@app.route('/', methods=["GET"])
+@app.route('/predict', methods=["GET"])
 def getData():
     my_processor.to_dataframes()
-    data = my_processor.merge_data()  # Put data into correct format for model
-
-    # Choose a random match number
     matches = my_processor.matches_processed()
     random_match_num = np.random.choice(matches)
-    curr_match = data[data == random_match_num]
+    curr_match = my_processor.players[my_processor.players['match_id'] == random_match_num]
+    match = curr_match.to_json(orient='records')  # Might need to update 'records' to 'columns'
+    return jsonify(match)  
 
-    # Get the match as a json and send to back end
-    match = curr_match.to_json(orient='records')
-    return jsonify(match)
-
-
-# Return the latest model test scores
-def scores():
-    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
+
